729cce24749b445fb34b043e45b6c6d868df6317
WaheedullahSaadi <136042838+WaheedullahSaadi@users.noreply.github.com>
2023-07-28 01:23:02 -0700

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,107 @@
+
+// function add(){
+//     addition+=1;
+//     document.getElementById('temp').innerText = addition;
+calculation='';
+calculatio=0;
+
+function showcal(){
+    calculation+='1';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number2(){
+    calculation+='2';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number3(){
+    calculation+='3';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number4(){
+    calculation+='4';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number5(){
+    calculation+='5';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number6(){
+    calculation+='6';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+
+function number7(){
+    calculation+='7';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number8(){
+    calculation+='8';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number9(){
+    calculation+='9';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;}
+
+    function number0(){
+        calculation+='0';
+        console.log(calculation);
+        document.getElementById('temp').innerHTML=calculation;
+}
+
+function number10(){
+    calculation+='+';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number11(){
+    calculation+='-';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number12(){
+    calculation+='*';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number13(){
+    calculation+='/';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number14(){
+    calculation+='.';
+    console.log(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number15(){
+    calculation+=eval(calculation);
+    document.getElementById('temp').innerHTML=calculation;
+}
+
+function number16(){
+    calculation+=0;
+    console.log(calculatio);
+    document.getElementById('temp').innerHTML=calculatio;
+}
